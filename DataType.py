ae5fb764ca47cf9d5fdd3bb17156f5bfa17381c2
GyubinLee <dev-coconut94@gmail.com>
2023-05-03 16:33:34 +0900

    Init Commit

---
+++ DataType.py
@@ -0,0 +1,72 @@
+##02-1 숫자형
+#integer
+a = 123
+print(a)
+a = -178
+print(a)
+a = 0
+print(a)
+
+#Floating-point
+a = 1.2
+print(a)
+a = -3.45
+print(a)
+
+#Octal
+a = 0o177
+print(a)
+
+#Hexadecimal
+a = 0x8ff
+b = 0xABC
+print(a)
+print(b)
+
+
+#Opertor
+a = 3
+b = 4
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+#Squre 제곱 "**"
+a = 3
+b = 4
+print(a ** b)
+
+#mob 나머지 "%"
+print(7%3)
+print(3%7)
+
+# 나눗셈 후 몫 반환 "//"
+print(7/4)
+print(7//4)
+
+##02-2 문자열
+"Hello World"
+
+'Python is fun'
+
+"""Life is too short, You need Python"""
+
+'''Life is too short, You need Python'''
+
+#문자열에 작은따옴표(') 포함시키기
+food = "Python's favorite food is perl"
+print(food)
+
+#문자열에 큰따옴표(") 포함시키기
+say = '"Python is very easy".he says.'
+
+#백슬래시(\)를 사용해서 작은따옴표(')와 큰따옴표(")를 문자열에 포함시키기
+fodd = 'Python\'s favorite food is perl'
+say = "\"Python is very easy.\" he says."
+
+#여러 줄인 문자열을 변수에 대입하고 싶을 때.
+#1.줄을 바꾸기 위한 이스케이프 코드 \n 삽입하기
+multiline = "life is too short\nYou need python"
+#2
+
